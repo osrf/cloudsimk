14cec718ebae3297c21791aef7192b2300d2d4c0
Ian Chen <ichen@osrfoundation.org>
2014-03-21 12:10:21 -0700

    add pagination

--- public/js/controllers/simulations.js
+++ public/js/controllers/simulations.js
@@ -25,24 +25,12 @@ angular.module('mean.simulations').controller('SimulationsController', ['$scope'
 
     $scope.find = function() {
         SimulationsRunning.query(function(simulations) {
-           //  var simulationItems = [];
-           //  for (var i = 0; i < simulations.length; ++i) {
-           //      var simulationItem = {};
-           //      simulationItem.selected = false;
-           //      simulationItem.id = simulations[i].sim_id;
-           //      simulationItem.world = simulations[i].world;
-           //      simulationItem.state = simulations[i].state;
-           //      simulationItem.region = simulations[i].region;
-           //      simulationItem.uptime = 0;
-           //      simulationItem.cost = 0;
-           //      simulationItems.push(simulationItem);
-           //  }
             $scope.simulations = simulations;
             for (var i = 0; i < simulations.length; ++i) {
                 simulations[i].selected = false;
             }
-
-            console.log("first sim " + $scope.simulations[0].region);
+            $scope.currentPage = 0;
+            $scope.groupToPages();
         });
     };
 
@@ -53,9 +41,59 @@ angular.module('mean.simulations').controller('SimulationsController', ['$scope'
             $scope.simulation = simulation;
         });
     };
-
     $scope.selectAllSimulations = function() {
         for (var i = 0; i < $scope.simulations.length; ++i)
             $scope.simulations[i].selected = $scope.simulationsChecked;
     };
+
+
+    $scope.gap = 5;
+    $scope.groupedItems = [];
+    $scope.itemsPerPage = 5;
+    $scope.pagedItems = [];
+    $scope.currentPage = 0;
+
+    $scope.groupToPages = function () {
+        $scope.pagedItems = [];
+        for (var i = 0; i < $scope.simulations.length; i++) {
+            if (i % $scope.itemsPerPage === 0) {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.simulations[i] ];
+            } else {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.simulations[i]);
+            }
+        }
+    };
+
+    $scope.range = function (size,start, end) {
+        var ret = [];
+        console.log(size,start, end);
+
+        if (size < end) {
+            end = size;
+            start = size-$scope.gap;
+        }
+        start = Math.max(start, 0);
+        for (var i = start; i < end; i++) {
+            ret.push(i);
+        }
+        console.log(ret);
+        return ret;
+    };
+
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 0) {
+            $scope.currentPage--;
+        }
+    };
+
+    $scope.nextPage = function () {
+        if ($scope.currentPage < $scope.pagedItems.length - 1) {
+            $scope.currentPage++;
+       }
+    };
+
+    $scope.setPage = function () {
+        $scope.currentPage = this.n;
+    };
+
 }]);
