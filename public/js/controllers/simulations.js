46a65332daebbbfaa79eb4bcac2f6cbc1814695e
Nate Koenig <natekoenig@gmail.com>
2014-03-24 08:27:30 -0700

    Use single service, and centralize the simulations controller

--- public/js/controllers/simulations.js
+++ public/js/controllers/simulations.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean.simulations').controller('SimulationsController', ['$scope', '$stateParams', '$location', 'Global', 'Simulations', 'SimulationsRunning', function ($scope, $stateParams, $location, Global, Simulations, SimulationsRunning) {
+angular.module('mean.simulations').controller('SimulationsController', ['$scope', '$stateParams', '$location', 'Global', 'Simulations', function ($scope, $stateParams, $location, Global, Simulations) {
     $scope.global = Global;
 
     /// All the worlds available to the user.
@@ -11,77 +11,24 @@ angular.module('mean.simulations').controller('SimulationsController', ['$scope'
     // TODO: Retrieve this list from the server.
     $scope.regions = ['US East', 'US West', 'Ireland'];
 
+    /// Get all the running simulations.
+    $scope.simulations = Simulations.query();
+
+    /// The current page of simulations
+    $scope.currentPage = 1;
+
+    /// Number of simulations to diplay per page
+    $scope.simPerPage = 5;
+
     /// Launch a simulation.
     $scope.launch = function() {
-
-        var simulation = new Simulations({
-            world: $scope.world,
-            region: $scope.region
+        var sim = new Simulations({
+            sim_id: 0,
+            state: 'Launching',
+            region: $scope.launch.region,
+            world: $scope.launch.world
         });
-        simulation.$save(function(response) {
-            $location.path('simulations/' + response._id);
-        });
+        sim.$save();
+        $scope.simulations.unshift(sim);
     };
-
-    $scope.find = function() {
-        SimulationsRunning.query(function(simulations) {
-            $scope.simulations = simulations;
-            for (var i = 0; i < simulations.length; ++i) {
-                simulations[i].selected = false;
-            }
-            $scope.currentPage = 0;
-            $scope.groupToPages();
-        });
-    };
-
-    $scope.findOne = function() {
-        Simulations.get({
-            sim_id: $stateParams.sim_id
-        }, function(simulation) {
-            $scope.simulation = simulation;
-        });
-    };
-    $scope.selectAllSimulations = function() {
-        for (var i = 0; i < $scope.simulations.length; ++i)
-            $scope.simulations[i].selected = $scope.simulationsChecked;
-    };
-
-    $scope.gap = 5;
-    $scope.groupedItems = [];
-    $scope.itemsPerPage = 5;
-    $scope.pagedItems = [];
-    $scope.currentPage = 0;
-
-    $scope.groupToPages = function () {
-        $scope.pagedItems = [];
-        for (var i = 0; i < $scope.simulations.length; i++) {
-            if (i % $scope.itemsPerPage === 0) {
-                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.simulations[i] ];
-            } else {
-                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.simulations[i]);
-            }
-        }
-    };
-
-    $scope.range = function (size,start, end) {
-        var ret = [];
-        // console.log(size,start, end);
-
-        if (size < end) {
-            end = size;
-            start = size-$scope.gap;
-        }
-        start = Math.max(start, 0);
-        for (var i = start; i < end; i++) {
-            ret.push(i);
-        }
-        // console.log(ret);
-        return ret;
-    };
-
-    $scope.setPage = function(n) {
-        if (n >= 0 && n < $scope.pagedItems.length)
-            $scope.currentPage = n;
-    };
-
 }]);
