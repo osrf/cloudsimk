c8fec4f4cb97778b4e3c6ac964730ad295dfeaf8
Nate Koenig <natekoenig@gmail.com>
2014-04-08 17:00:47 -0700

    Made a generic alert directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,1 +1,10 @@
 'use strict';
+
+/////////////////////////////////////////////////
+// Displays a boost-style error message
+angular.module('cloudsim').directive('cloudsimAlert', function() {
+    return {
+       restrict: 'E',
+       templateUrl: '/views/templates/alert.html'
+    };
+});
